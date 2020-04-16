4979b44cf80f753fe930b01f70d2d9da6b65f3c3
punisher03 <rshk70@gmail.com>
2020-04-16 06:11:46 +0530

    week 1

--- SingleNumber.py
+++ SingleNumber.py
@@ -1,3 +1,8 @@
 #Given a non-empty array of integers, every element appears twice except for one. Find that single one.
 
 #using math
+
+def check(a):
+    s=sum(set(a))
+    return 2*s-sum(a)
+print(check([1,1,2,2,3,4,4,5,5]))
